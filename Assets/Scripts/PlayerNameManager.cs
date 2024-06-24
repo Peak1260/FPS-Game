18b1b3390cd64018c531e2a03fe6375b37e4c302
Pitchaya Manopchantaroj <pmanopchantaroj@ucsb.edu>
2024-06-23 19:16:17 -0700

    adding files for fps game

---
+++ Assets/Scripts/PlayerNameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerNameManager : MonoBehaviour
+{
+    [SerializeField] TMP_InputField usernameInput;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("username"))
+        {
+            usernameInput.text = PlayerPrefs.GetString("username");
+            PhotonNetwork.NickName = PlayerPrefs.GetString("username");
+        }
+        else
+        {
+            usernameInput.text = "Player " + Random.Range(0, 100).ToString("00");
+            OnUserNameInputValueChanged();
+        }
+    }
+
+    public void OnUserNameInputValueChanged()
+    {
+        PhotonNetwork.NickName = usernameInput.text;
+        PlayerPrefs.SetString("username", usernameInput.text);
+    }
+}
